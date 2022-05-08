3a533a078d67d6945139cba63e8d2878c60cf62d
sergeiivanow <38918636+sergeiivanow@users.noreply.github.com>
2022-05-08 20:54:58 +0300

    Revert "Remove onCreateNode"
    This reverts commit b0829f94f4c37a6fa8580dd0f3d85b706908d2d3.

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,6 +58,20 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   }
 }
 
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
 
